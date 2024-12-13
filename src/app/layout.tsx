3174cfce2f0218fab9c7c52c59247bc14192c6af
kouyaruten <williamjinq@outlook.com>
2024-12-13 21:56:35 +0800

    Enhance project dependencies and layout with Vercel Analytics integration
    - Added @vercel/analytics package to package.json for improved analytics tracking.
    - Updated pnpm-lock.yaml to reflect changes in dependencies and their versions.
    - Refactored layout.tsx to include the Analytics component for tracking user interactions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: 'Spotpaper',
-  description: 'Get high-quality wallpapers from Spotify albums',
+  title: "Spotpaper",
+  description: "Get high-quality wallpapers from Spotify albums",
 };
 
 export default function RootLayout({
@@ -29,6 +30,7 @@ export default function RootLayout({
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
